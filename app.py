bf2ebed5c729fd05166a6a25cc0c7ccca24d2d42
sidddddd-18 <siddharth.sadhwani@gmail.com>
2020-04-26 03:49:21 +0530

    Automated testing

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import nltk
 from nltk.corpus import stopwords
 import lxml
 from flask import Flask, render_template,request,flash
+import json
 
 REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
 STOPWORDS=set(stopwords.words('english'))
@@ -36,63 +37,78 @@ def home():
 
 	if request.method == "POST":
 		link=request.form["URL"]
-		try:
-				sub=reddit.submission(url=str(link))
-		except:
-			flash("Try again")
+		result=predict(link)
+		if result=="Url not valid" or result=="Post does not belong to r/india":
+			flash(result)
 			return render_template("index.html")
-		if str(sub.subreddit)!="india":
-			flash("Post does not belong to r/india")
+		else :
+			flash("The flair is : "+result)
 			return render_template("index.html")
-		posts=[]
-		title=sub.title
-		url=sub.url
-		body=sub.selftext
-		sub.comments.replace_more(limit=0)
-		st=''
-		for c in sub.comments.list():
-			count=0
-			if count<=30:
-				st=st+c.body+' '
-				count=+1
-			else:
-				break
-		comments=st
-		posts.append([title,url,body,comments])
-		data=pd.DataFrame(posts,columns=['title','url','body','comments'])
-		title=sub.title
-		url=sub.url
-		body=sub.selftext
-		sub.comments.replace_more(limit=0)
-		st=''
-		for c in sub.comments.list():
-			count=0
-			if count<=30:
-				st=st+c.body+' '
-				count=+1
-			else:
-				break
-		comments=st
-		posts.append([title,url,body,comments])
-		data=pd.DataFrame(posts,columns=['title','url','body','comments'])
-		data["title"]=data["title"].apply(string_form)
-		data["body"]=data["body"].apply(string_form)
-		data["comments"]=data["comments"].apply(string_form)
-
-		data["title"]=data["title"].apply(clean_text)
-		data["body"]=data["body"].apply(clean_text)
-		data["comments"]=data["comments"].apply(clean_text)
-
-		combination=data["comments"]+data["title"]+data["url"]+data["body"]
-
-
-		loaded_model=pickle.load(open("model/SVM_ctbu.sav",'rb'))
-		predicted_flair=loaded_model.predict(combination)
-		result=str(predicted_flair)
-
-		flash("The flair is : "+result[2:(len(result)//2)-1])
-		return render_template("index.html")
 	return render_template("index.html")
+
+@app.route('/automated_testing',method=['POST'])
+def automated_testing():
+	file=request.form['upload_file']
+	links=file.read().decode('utf-8').split('\n')
+	results={}
+	for i in links:
+		result=predict(i)
+		results[link] = result
+	return json.dumps(results)
+
+
+def predict(link):
+	try:
+		sub=reddit.submission(url=str(link))
+	except:
+		error="Url not valid"
+		return error
+	if str(sub.subreddit)!="india":
+		error="Post does not belong to r/india"
+		return error
+	posts=[]
+	title=sub.title
+	url=sub.url
+	body=sub.selftext
+	sub.comments.replace_more(limit=0)
+	st=''
+	for c in sub.comments.list():
+		count=0
+		if count<=30:
+			st=st+c.body+' '
+			count=+1
+		else:
+			break
+	comments=st
+	posts.append([title,url,body,comments])
+	data=pd.DataFrame(posts,columns=['title','url','body','comments'])
+	title=sub.title
+	url=sub.url
+	body=sub.selftext
+	sub.comments.replace_more(limit=0)
+	st=''
+	for c in sub.comments.list():
+		count=0
+		if count<=30:
+			st=st+c.body+' '
+			count=+1
+		else:
+			break
+	comments=st
+	posts.append([title,url,body,comments])
+	data=pd.DataFrame(posts,columns=['title','url','body','comments'])
+	data["title"]=data["title"].apply(string_form)
+	data["body"]=data["body"].apply(string_form)
+	data["comments"]=data["comments"].apply(string_form)
+	data["title"]=data["title"].apply(clean_text)
+	data["body"]=data["body"].apply(clean_text)
+	data["comments"]=data["comments"].apply(clean_text)
+	combination=data["comments"]+data["title"]+data["url"]+data["body"]
+
+	loaded_model=pickle.load(open("model/SVM_ctbu.sav",'rb'))
+	predicted_flair=loaded_model.predict(combination)
+	result=str(predicted_flair)
+	return result[2:(len(result)//2)-1]
 
 if __name__ == '__main__':
 	app.run()    
